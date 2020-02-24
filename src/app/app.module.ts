5886708fe68e71a92ec5fa0091f4d749fed97533
Kurt Opel <kurt@kurtopel.com>
2020-02-24 15:18:04 -0500

    added modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { FormsModule } from '@angular/forms';
 import {AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodoAddComponent } from './todo-add/todo-add.component';
+import { TodoEditComponent } from './todo-edit/todo-edit.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, TodoComponent, TodoAddComponent, TodoEditComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
